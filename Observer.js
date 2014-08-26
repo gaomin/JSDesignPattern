088be74eb46503ade18268f9354a9aa1af07eae6
gaomin <gaominwangyi@163.com>
2014-08-26 11:56:59 +0800

    Observer.js

--- Observer.js
+++ Observer.js
@@ -132,4 +132,76 @@ o.update("Tom回来了！")
 //退订f1
 o.unsubscribe(f1);
 //再来验证
-o.update("Tom回来了！");   
+o.update("Tom回来了！");  
+
+//version_3
+//让多个对象都具有观察者发布订阅的功能，可以定义一个通用的函数，然后将函数的功能应用到观察者功能的对象上
+var observer = {
+	addSubscriber : function(callback){
+		this.subscribers[this.subscribers.length]  = callback;
+	},
+	removeSubscriber : function(callback){
+		for(var i = 0; i < this.subscribers.length; i++){
+			if(this.subscribers[i] === callback){
+				delete(this.subscribers[i]);
+			}
+		}
+	},
+	publish : function(what){
+		for(var i = 0; i < this.subscribers.length; i++){
+			if(typeof this.subscribers[i] === 'function'){
+				this.subscribers[i](what);
+			}
+		}
+	},
+
+	// 将对象o 具有观察者功能
+	make : function(o){
+		for(var i in this){
+			o[i] = this[i];
+			o.subscribers = [];
+		}
+	}
+};
+//订阅2个对象blogger和user，使用observer.make方法将这2个对象具有观察者功能
+
+var blogger = {
+	recommend : function(id){
+		var msg = "dudu 推荐了的帖子："+ id;
+		this.publish(msg);
+	}
+};
+
+var user = {
+    vote: function (id) {
+        var msg = '有人投票了!ID=' + id;
+        this.publish(msg);
+    }
+};
+
+observer.make(blogger);
+observer.make(user);
+//订阅不同的回调函数，以便可以注册到不同的观察者对象里（也可以同时注册到多个观察者对象里）
+var tom = {
+    read: function (what) {
+        console.log('Tom看到了如下信息：' + what)
+    }
+};
+
+var mm = {
+    show: function (what) {
+        console.log('mm看到了如下信息：' + what)
+    }
+};
+// 订阅
+blogger.addSubscriber(tom.read);
+blogger.addSubscriber(mm.show);
+blogger.recommend(123); //调用发布
+
+//退订
+blogger.removeSubscriber(mm.show);
+blogger.recommend(456); //调用发布
+
+//另外一个对象的订阅
+user.addSubscriber(mm.show);
+user.vote(789); //调用发布
