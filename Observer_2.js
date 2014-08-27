5c5fb9d2dd124b982b011f8457475ea2779ad511
gaomin <gaominwangyi@163.com>
2014-08-27 16:50:09 +0800

    Observer_2.js

--- Observer_2.js
+++ Observer_2.js
@@ -1,3 +1,4 @@
+// e.g.1  ??
 // Observer
 function ObserverList(){
 	this.observerList = [];
@@ -41,9 +42,74 @@ ObserverList.prototype = {
 	},
 	RemoveIndexAt : function(index){
 		if(index === 0){
+			this.observerList.shift();
+		}else if(index === this.observerList.length - 1){
 			this.observerList.pop();
 		}
 	}
+};
 
-
+//使用extension扩展对象
+function extend(obj,extension){
+	for(var key in obj){
+		extension[key] = obj[key];
+	}
 }
+
+//Subject
+
+function Subject(){
+	this.observers = new ObserverList();
+}
+
+Subject.prototype = {
+	AddObserver : function(observer){
+		this.observers.Add(observer);
+	},
+	RemoveObserver : function(observer){
+		this.observers.RemoveIndexAt(this.observers.IndexOf(observer,0));
+	},
+	Notify : function(context){
+		var observerCount = this.observers.Count();
+		for( var i = 0; i < observerCount; i++){
+			this.observers.Get(i).Update(context);
+		}
+	}
+};
+
+//Observer
+function Observer(){
+	this.Update = function(){}
+}
+
+//HTML
+// <button id="addNewObserver">Add New Observer checkbox </button>
+// <input id="mainCheckbox" type="checkbox"/>
+// <div id="observersContainer"></div>
+
+var controlCheckbox = document.getElementById("mainCheckbox"),
+	addBtn = document.getElementById("addNewObserver"),
+	container = document.getElementById("observersContainer");
+
+// 具体目标 Concrete Subject
+extend(new Subject(), controlCheckbox);
+
+controlCheckbox['onclick'] = new Function("controlCheckbox.Notify(controlCheckbox.checked)");
+addBtn["onclick"] = addNewObserver;
+
+// 具体观察者 Concrete Observer
+
+function AddNewObserver(){
+	var check = document.createElement("input");
+	check.type = "checkbox";
+
+	extend(new Observer(), check);
+	check.Update = function(value){
+		this.checked = value;
+	};
+
+	controlCheckbox.AddObserver(check);
+	container.appendChild(check);
+}
+
+
